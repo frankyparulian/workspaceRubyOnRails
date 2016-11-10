77dc19795560f7503b91b6fbfe0682cf39017ed5
frankyparulian <frankyparulian@gmail.com>
2016-11-10 12:41:29 +0700

    commit code

---
+++ reactjs_ruby/app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,13 @@
+varAllItems =  React.createClass({
+    componentDidMount(){
+        console.log('component mounted');
+    },
+
+    render(){
+      return (
+        <div>
+            <h1>All items Component</h1>
+        </div>
+      )
+    }
+});
